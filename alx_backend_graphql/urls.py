91f49479b3fdac499efe718796ec8182ea45e6c0
Andee Olayinkascott <andeeolayinkascott@gmail.com>
2025-10-28 23:05:40 +0100

    Move schema file

--- alx_backend_graphql/urls.py
+++ alx_backend_graphql/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
-from crm.schema import schema
+from alx_backend_graphql.schema import schema
 
 urlpatterns = [
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
