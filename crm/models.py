5a703609d867e9778af25dbb962c4b2ccc0a24d9
Andee Olayinkascott <andeeolayinkascott@gmail.com>
2025-11-04 10:15:23 +0100

    Add str method to models

--- crm/models.py
+++ crm/models.py
@@ -3,19 +3,25 @@ from django.core.validators import MinValueValidator
 
 
 class Customer(models.Model):
-    name = models.CharField(max_length=75)
+    name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=12, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
-    name = models.CharField(max_length=75)
+    name = models.CharField(max_length=100)
     price = models.DecimalField(
         max_digits=10,
         decimal_places=2,
         validators=[
             MinValueValidator(0)])
     stock = models.PositiveIntegerField(default=0, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
